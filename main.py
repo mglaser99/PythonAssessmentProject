57d76f244b9585416f3d658cc37a2f97c306a4cf
mglaser99 <melanie.glaser2@uzh.ch>
2026-05-07 11:07:40 +0200

    Add embedding model to fastapi app
    find_title now returns the most similar title from the provided input

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+from EmbeddingModel import allMiniLM
 
 class Titles(BaseModel):
     reference: str
     other: list[str]
 
+model = allMiniLM()
+
 app = FastAPI()
 
 @app.post("/")
 async def find_title(titles: Titles):
-    return {"top_result": titles.reference}
+    result = model.most_similar(titles.reference, titles.other)
+    return {"top_result": result[0]}
 
 
 if __name__=="__main__":
